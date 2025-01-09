1f483c5f49cdc42fdf76620e449c036e9c92abda
Mink <mink4sure@kpnmail.nl>
2025-01-09 17:04:28 +0100

    Updated notes

--- my_julia_project/src/simplest_case.jl
+++ my_julia_project/src/simplest_case.jl
@@ -14,5 +14,44 @@ println("Lets begin!!")
 
 using RxInfer, Random
 
-rng = MersenneTwister(42)
+@model function one_dim_model(y, size, T, unobserved_prior)
+  # Declare the state variable `x` as a random variable array with size `(size, length(y))`
+
+  # Define the transition of the state
+  for i in 1:size
+    x[i][1] ~ Bernoulli(unobserved_prior)  # Initial state
+
+    for t in 2:T
+      x[i, t] := x[i, t - 1]  # Transition (here, simply copying the previous state)
+      end
+  end
+
+  # Define the observation model
+  for i in 1:size
+    for t in 1:length(y)
+      y[i, t] ~ Normal(mean = x[i, t], variance = 0.1)
+    end
+  end
+end
+
+println("Model defined")
+println("Starting inference")
+
+dataset = [[0, 0, 0, 0, 0],
+           [0, 1, 1, 1, 1],
+           [0, 0, 0, 0, 0]]
+
+result = infer(
+  model = one_dim_model(size = 3, unobserved_prior = 0.5),
+  data = (y = dataset,)
+)
+
+x_estimated = result.posteriors[:x]  
+x11 = x_estimated[1, 1]
+x15 = x_estimated[1, 5]
+x23 = x_estimated[2, 3]
+
+println(x11)
+println(x15)
+println(x23)
 
